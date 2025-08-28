fc12d18ad077e94557fb0fcf9f47c4ddcd44c53e
Yash Burshe <bursheyash2@gmail.com>
2025-08-28 12:24:23 -0400

    Add contact details

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
@@ -8,6 +9,14 @@ export default function Home() {
         <h1>Code Smells Reference</h1>
         <p>Welcome to the code smells reference website.</p>
         <p>To get started, click any code smell in the navbar.</p>
+        <hr/>
+        <section>
+          <p>For any queries, please contact</p>
+          <ul>
+            <li>Layla Dong at <Link href="mailto:dong.pa@northeastern.edu">dong.pa@northeastern.edu</Link></li>
+            <li>Yash Mahesh Burshe at <Link href="mailto:burshe.y@northeastern.edu">burshe.y@northeastern.edu</Link></li>
+          </ul>
+        </section>
       </main>
     </div>
   );
